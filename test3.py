b596903c3b88d0b7032b867b54b0655c4aaf2001
julien-mcx <123368888+julien-mcx@users.noreply.github.com>
2023-02-12 18:23:41 +0100

    Update test3.py

--- test3.py
+++ test3.py
@@ -1,3 +1,7 @@
 import streamlit as st 
+import naas_drivers
+from naas_drivers import gsheet
 
-st.write("hello world!")
+SPREADSHEET_URL = "https://docs.google.com//spreadsheets//d//1yQCZqRweD7FLCAEFZagH00WmSQ1Pb8iVWTTnC6X3jLM//edit#gid=452671349"
+SHEET_NAME = "FuturesForwards"
+df = gsheet.connect(SPREADSHEET_URL)#.get(sheet_name=SHEET_NAME)
