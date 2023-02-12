8eafc1da452304b794629e7c45c57d3f15e3b42f
julien-mcx <123368888+julien-mcx@users.noreply.github.com>
2023-02-12 19:19:45 +0100

    Update test3.py

--- test3.py
+++ test3.py
@@ -18,5 +18,5 @@ scope = ['https://www.googleapis.com/auth/spreadsheets',
 credentials = ServiceAccountCredentials.from_json_keyfile_name("streamlit1202-240ada1c5101.json", scope)
 client = gspread.authorize(credentials)
 
-sheet = client.open("FuturesForwards")
+sheet = client.open("QuestionsFinancedeMarché").sheet1
 st.write(sheet)
