2d6220794cad029df82e236c431f191747fc472f
julien-mcx <123368888+julien-mcx@users.noreply.github.com>
2023-02-12 18:26:01 +0100

    Update test3.py

--- test3.py
+++ test3.py
@@ -5,3 +5,4 @@ from naas_drivers import gsheet
 SPREADSHEET_URL = "https://docs.google.com//spreadsheets//d//1yQCZqRweD7FLCAEFZagH00WmSQ1Pb8iVWTTnC6X3jLM//edit#gid=452671349"
 SHEET_NAME = "FuturesForwards"
 df = gsheet.connect(SPREADSHEET_URL)#.get(sheet_name=SHEET_NAME)
+st.write(df)
