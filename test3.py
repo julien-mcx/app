a93a7be410adf9cc7aec3d63abc333a247bfed61
julien-mcx <123368888+julien-mcx@users.noreply.github.com>
2023-02-13 19:04:48 +0100

    Update test3.py

--- test3.py
+++ test3.py
@@ -24,21 +24,3 @@ gc = gspread.service_account()
 sh = gc.open("QuestionsFinancedeMarché")
 st.write(sh.sheet1.get('A1')
 
-
-
-# # client.open("QuestionsFinancedeMarché").sheet1
-# Sheet = client.open_by_key("1HMZGbomaWzYrg3V0Hx3sRvmL6g3EFOMRac_ZbrRC32Y").sheet1
-# # st.write(Sheet.shape[0])
-
-# sheet = client.create("NewDatabase")
-# st.write(sheet)
-
-# sh = client.open("Commentaires") # Open by name
-
-
-# sh = client.open_by_url('https://docs.google.com/spreadsheets/d/1yQCZqRweD7FLCAEFZagH00WmSQ1Pb8iVWTTnC6X3jLM/edit#gid=452671349') # Open by URL
-# st.write(sh)
-
-# worksheet = client.get_worksheet(0) # Select worksheet by index
-
-
