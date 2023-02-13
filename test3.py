ea5e9c0f37bcd821397ca3e10b4aa4429e15dca3
julien-mcx <123368888+julien-mcx@users.noreply.github.com>
2023-02-13 18:46:07 +0100

    Update test3.py

--- test3.py
+++ test3.py
@@ -25,7 +25,7 @@ client = gspread.authorize(credentials)
 # sheet = client.create("NewDatabase")
 # st.write(sheet)
 
-sh = client.open(Commentaires") # Open by name
+sh = client.open("Commentaires") # Open by name
 
 
 # sh = client.open_by_url('https://docs.google.com/spreadsheets/d/1yQCZqRweD7FLCAEFZagH00WmSQ1Pb8iVWTTnC6X3jLM/edit#gid=452671349') # Open by URL
